016f63d1e22ee7b097f6f7b54b9e0c6f13216710
Yaroslav Rosnovskiy <rosn.yaroslav@gmail.com>
2024-02-21 06:39:49 +0200

    stars and form added

--- greenhub/src/app/components/general/successfull-order/successfull-order.component.ts
+++ greenhub/src/app/components/general/successfull-order/successfull-order.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./successfull-order.component.scss']
 })
 export class SuccessfullOrderComponent {
+  starsArray: number[] = [1, 2, 3, 4, 5];
+  hoveredStarIndex: number | null = null;
+  clickedStarIndex: number | null = null;
+  starIcons: string[] = [];
+  reviewText: string = '';
+  starRating: number | null = null;
 
+  constructor() {
+    this.starIcons = this.starsArray.map(() => '../../../assets/star.svg');
+  }
+
+  highlightStars(index: number): void {
+    if (this.clickedStarIndex !== null) {
+      this.hoveredStarIndex = index;
+      for (let i = 0; i <= this.clickedStarIndex; i++) {
+        this.starIcons[i] = '../../../assets/star-fill.svg';
+      }
+    } else {
+      this.hoveredStarIndex = index;
+      for (let i = 0; i <= index; i++) {
+        this.starIcons[i] = '../../../assets/star-fill.svg';
+      }
+    }
+  }
+
+  resetStars(): void {
+    for (let i = 0; i < this.starsArray.length; i++) {
+      if (this.clickedStarIndex !== null && i <= this.clickedStarIndex) {
+        this.starIcons[i] = '../../../assets/star-fill.svg';
+      } else {
+        this.starIcons[i] = '../../../assets/star.svg';
+      }
+    }
+    this.hoveredStarIndex = null;
+  }
+
+  logStar(index: number): void {
+    this.clickedStarIndex = index;
+    this.highlightStars(index);
+    this.starRating = index + 1;
+  }
+
+  onSubmit(): void {
+    console.log('Review Text:', this.reviewText);
+    console.log('Star Rating:', this.starRating);
+    
+  }
 }
